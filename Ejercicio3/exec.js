3bdf11e07d9caf660a2441188d2d967ffc7d67da
Facundo Apesteguia <43476980+fapesteguia@users.noreply.github.com>
2021-03-30 23:36:48 -0300

    Ejercicio3

---
+++ Ejercicio3/exec.js
@@ -0,0 +1,8 @@
+let palabra = prompt('Ingrese palabra a repetir');
+let i = 0;
+let CantidadRepeticion = prompt('Ingrese cantidad de veces a repetir');
+
+while (i < parseInt(CantidadRepeticion)) {
+    console.log(palabra);
+    i++;
+}
