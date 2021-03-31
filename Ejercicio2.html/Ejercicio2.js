bf8216351b2381bca35153240ce65be16586843f
Facundo Apesteguia <43476980+fapesteguia@users.noreply.github.com>
2021-03-30 23:26:41 -0300

    commit

--- Ejercicio2.html/Ejercicio2.js
+++ Ejercicio2.html/Ejercicio2.js
@@ -1,1 +1,26 @@
-console.log('No podes ingresar cero');
+//Primer ejemplo
+
+let numero = prompt('Ingrese numero entre 10 y 50');
+
+
+parseInt(numero)
+if (parseInt(numero) > 10 && parseInt(numero) < 50) {
+
+    console.log(' El numero esta entre 10 y 50');
+} else {
+
+    console.log('El numero no esta entre 10 y 50')
+};
+
+
+
+//Otro ejemplo
+
+let nombre = prompt('Ingrese Nombre');
+let cursada = 12345;
+
+if (nombre === 'Facundo' || nombre === 'facundo' && cursada === 12345) {
+    console.log('Facundo tu cursada correspondiente es 12345')
+} else {
+    console.log('Comunicate con el departamento de alumnos, no podemos encontrar tu cursada!')
+}
